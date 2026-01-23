cb8c66042615afb6b32d4f95371f6178171bc756
josip <josipkeresman@gmail.com>
2026-01-23 23:01:36 +0100

    Fix formatting in comment entity (#25)

--- models/comment.go
+++ models/comment.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Comment struct {
 	gorm.Model
-	Content  string `gorm:"type:text;not null"`
-	UserID   uint   `gorm:"not null"`
-	NewsID   uint   `gorm:"not null"`
+	Content string `gorm:"type:text;not null"`
+	UserID  uint   `gorm:"not null"`
+	NewsID  uint   `gorm:"not null"`
 
 	User User `gorm:"foreignKey:UserID"`
 	News News `gorm:"foreignKey:NewsID"`
